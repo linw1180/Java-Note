17a21369037ab1c92efcc87d3a59ce62a51efa70
linw1180 <19970819lww>
2021-11-02 23:35:54 +0800

    feat：冒泡排序

---
+++ src/com/luis/note/BubbleSort.java
@@ -0,0 +1,36 @@
+package com.luis.note;
+
+/**
+ * @Author: Luis
+ * @date: 2021/11/2 22:50
+ * @description: 冒泡排序
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        /*
+            冒泡排序原理：进行多次排序，每次排序完，将最大值放到最右边
+            每次排序方法：将前一个数和后一个数进行比较，前数大于后数，则交换位置，每次都能将最大值放到最右边
+            注意：每次放到最右边的最大值不参与下次比较
+         */
+        int[] arr = {4, 1, 6, 2, 3, 7, 0};
+        // 7 个数，需要进行 6 次排序
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 第 1 次排序：交换 6 次
+            // 第 2 次排序：交换 5 次
+            // 第 3 次排序：交换 4 次
+            // 以此类推...
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        // 遍历排序后的数组
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
+        }
+    }
+}
